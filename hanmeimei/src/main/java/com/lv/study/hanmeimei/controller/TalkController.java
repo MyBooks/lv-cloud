028fa270eda698783189db24a8a8324988e47777
吕孝强 <1154712196@qq.com>
2020-11-20 06:59:13 +0800

    从求中获取head中的UUID

--- hanmeimei/src/main/java/com/lv/study/hanmeimei/controller/TalkController.java
+++ hanmeimei/src/main/java/com/lv/study/hanmeimei/controller/TalkController.java
@@ -1,11 +1,15 @@
 package com.lv.study.hanmeimei.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.apache.tomcat.util.http.fileupload.RequestContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 
 @RestController
@@ -15,9 +19,11 @@ public class TalkController {
     @HystrixCommand(fallbackMethod = "error")
     @RequestMapping(method = RequestMethod.GET,path = "sayHello")
     public String sayHello(){
-        RequestAttributes requestAttributes = RequestContextHolder.currentRequestAttributes();
-        System.out.println("requestAttributes = " + requestAttributes);
-        return "hello";
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes)RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        String uuid = request.getHeader("UUID");
+        System.out.println("uuid:" + uuid);
+        return "hello uuid:"+uuid;
     }
 
     public String error(){
