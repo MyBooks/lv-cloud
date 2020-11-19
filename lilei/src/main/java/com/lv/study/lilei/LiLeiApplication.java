b5883c1a7392e074c2c359933a1bdc4a1e8b381b
吕孝强 <1154712196@qq.com>
2020-11-20 07:29:11 +0800

    新增李雷服务器

---
+++ lilei/src/main/java/com/lv/study/lilei/LiLeiApplication.java
@@ -0,0 +1,13 @@
+package com.lv.study.lilei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class LiLeiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LiLeiApplication.class,args);
+    }
+}
