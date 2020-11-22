b36412688a71b7dfb8348c266b2ab92602a7b133
吕孝强 <1154712196@qq.com>
2020-11-22 13:57:16 +0800

    log4j2和logback使用

--- hanmeimei/src/main/java/com/lv/study/hanmeimei/controller/TalkController.java
+++ hanmeimei/src/main/java/com/lv/study/hanmeimei/controller/TalkController.java
@@ -1,13 +1,13 @@
 package com.lv.study.hanmeimei.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import org.apache.tomcat.util.http.fileupload.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -17,6 +17,8 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @RequestMapping("talk")
 public class TalkController {
+    static final Logger log = LoggerFactory.getLogger(TalkController.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
@@ -26,7 +28,7 @@ public class TalkController {
         ServletRequestAttributes requestAttributes = (ServletRequestAttributes)RequestContextHolder.currentRequestAttributes();
         HttpServletRequest request = requestAttributes.getRequest();
         String uuid = request.getHeader("UUID");
-        System.out.println("uuid:" + uuid);
+        log.info("UUID:{}",uuid);
         return "hello uuid:"+uuid;
     }
 
@@ -34,10 +36,12 @@ public class TalkController {
     @RequestMapping(method = RequestMethod.GET,path = "talkToLiLei")
     public String talkToLiLei(){
         String forObject = restTemplate.getForObject("http://LiLeiServer/talk/hiLiLei", String.class);
+        log.info(forObject);
         return forObject;
     }
 
     public String error(){
+        log.error("sayHello|talkToLiLei fail");
         return "sorry~~~";
     }
 }
